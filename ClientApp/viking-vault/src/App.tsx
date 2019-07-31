87775ee7aa2f1bfa8394f9494406ebc00c07b2dc
Robert Pintilii <robert.pin9@gmail.com>
2019-07-31 11:55:05 +0300

    Added routes

--- ClientApp/viking-vault/src/App.tsx
+++ ClientApp/viking-vault/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { RegisterForm } from "./RegisterForm";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { ResponseModal } from './ReponseModal';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <RegisterForm />
-    </div>
+    <Router>
+      <Route path="/register/" exact component={RegisterForm} />
+      {/* <Route path="/login/" exact component={LoginComponent}/> */}
+      <div className="App">
+      </div>
+    </Router>
   );
 }
 
