2340f01669ed34de977b1f45f1e9ffee6afee3b3
moleguaco99 <dragos_serban2000@yahoo.com>
2019-08-01 15:55:30 +0300

    Fixed routing

--- ClientApp/viking-vault/src/App.tsx
+++ ClientApp/viking-vault/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { RegisterForm } from "./RegisterForm";
+import {LoginForm} from "./Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import UserPage from './components/UserPage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
         <Route path="/register/" exact component={RegisterForm} />
+        <Route path="/login/" exact component={LoginForm}/>
+        <Route path="/user" component={UserPage} />
       </Router>
     </div>
-    
   );
 }
 
