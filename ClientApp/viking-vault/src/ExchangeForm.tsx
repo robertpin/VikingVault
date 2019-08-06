53f6cc39aff08675bc7f36f40ebb6b52574dc5eb
Olah Catalina <olah_catalina@yahoo.com>
2019-08-06 16:09:09 +0300

    Used exchange API to display conversion rates

--- ClientApp/viking-vault/src/ExchangeForm.tsx
+++ ClientApp/viking-vault/src/ExchangeForm.tsx
@@ -2,6 +2,7 @@ import './ExchangeForm.css';
 import React from "react";
 import SideBar from './components/SideBar';
 import TopBar from './components/TopBar';
+import { throwStatement } from '@babel/types';
 
 
 interface IExchangeFormState {
@@ -9,6 +10,11 @@ interface IExchangeFormState {
     toCurrency: string;
     availableAmount: Number;
     toExchangeAmount: Number;
+    ronToEur: Number;
+    ronToUsd: Number;
+    ronToYen: Number;
+    maximumValueToBeChanged: Number;
+
 }
 
 class ExchangeForm extends React.Component<any, IExchangeFormState> {
@@ -19,8 +25,54 @@ class ExchangeForm extends React.Component<any, IExchangeFormState> {
             fromCurrency: "RON",
             toCurrency: "EUR",
             availableAmount: 0.00,
-            toExchangeAmount: 0.00
+            toExchangeAmount: 0.00,
+            ronToEur: 0.00,
+            ronToUsd: 0.00,
+            ronToYen: 0.00,
+            maximumValueToBeChanged: 0.00
         }
+    }
+
+    getMaximumValueToBeChanged() {
+        // pe asta tre s-o iau din baza de date, din accounts
+    }
+
+    exchange(){
+    }
+
+    getCurrencyRates() {
+        fetch('https://api.exchangeratesapi.io/latest?base=RON')
+        .then(response => response.json())
+        .then(data => this.setState({ 
+            ronToEur: data.rates.EUR.toPrecision(3),
+            ronToUsd: data.rates.USD.toPrecision(3),
+            ronToYen: data.rates.JPY.toPrecision(3)
+         }));
+    }
+ 
+    componentDidMount() {
+        this.getCurrencyRates();
+        setInterval(() => {
+            this.getCurrencyRates();
+        }, 5000);
+    }
+
+    setCurrencyToExchangeIn = (e:any) => {
+        this.setState({
+            toCurrency: e.target.value
+        });
+    }
+
+    setCurrencyToBeExchanged = (e:any) => {
+        this.setState({
+            fromCurrency: e.target.value
+        });
+    }
+
+    setAmountOfMoneyToBeChanged = (e:any) => {
+        this.setState({
+            toExchangeAmount: e.target.value
+        });
     }
 
     render() {
@@ -28,14 +80,15 @@ class ExchangeForm extends React.Component<any, IExchangeFormState> {
             <div>
                 <SideBar/>
                 <TopBar/>
-                <div className="exchange-page-text">Exchange page</div>
+                <div className="exchange-page-text">Exchange</div>
                 <div className="row exchange-container">
                     <div className="col">
                         <p className="text-decoration">From:</p>
-                        <select className="form-control input-field">
-                            <option value="1">EUR</option>
-                            <option value="2">LEI</option>
-                            <option value="3">YEN</option>
+                        <select className="form-control input-field" onChange={this.setCurrencyToBeExchanged}>
+                            <option value="EUR">EUR</option>
+                            <option value="RON">RON</option>
+                            <option value="YEN">YEN</option>
+                            <option value="USD">USD</option>
                         </select>
                         <div className="badge badge-dark available-amount-div">
                             Available
@@ -43,32 +96,33 @@ class ExchangeForm extends React.Component<any, IExchangeFormState> {
                             {this.state.availableAmount.toPrecision(3)} {this.state.fromCurrency}
                         </div>
                         <p className="text-decoration">To:</p>
-                        <select className="form-control input-field">
-                            <option value="1">EUR</option>
-                            <option value="2">LEU</option>
-                            <option value="3">YEN</option>
-                            <option value="4">USD</option>
+                        <select className="form-control input-field" onChange={this.setCurrencyToExchangeIn}>
+                            <option value="EUR">EUR</option>
+                            <option value="RON">RON</option>
+                            <option value="YEN">YEN</option>
+                            <option value="USD">USD</option>
                         </select>
                         <p className="text-decoration">Amount to change:</p>
                         <input
                                 className="form-control input-field"
                                 placeholder="Amount..."
                                 type="number"
-                                min="0.01"
-                                max="100"
+                                min="1"
+                                max={this.state.maximumValueToBeChanged.toString()}
                                 step="0.01"
+                                onChange={this.setAmountOfMoneyToBeChanged}
                         />
                         <span className="badge badge-info fee-text">Fee today:</span>
-                        <button id="exchange-button" className="btn btn-primary">Exchange!</button>
+                        <button id="exchange-button" className="btn btn-primary" onClick={this.exchange}>Exchange!</button>
                     </div>
                     <div id="conversion-div" className="col-3">
                         <h3>Conversion rates</h3>
                         Conversion rates for all available currencies.<br/><br/>
-                        <h3>1 Romanian LEU</h3>
+                        <h3>1 Romanian RON</h3>
                         <div>
-                            = .... EUR <br/>
-                            = .... YEN <br/>
-                            = .... USD
+                            = {this.state.ronToEur} EUR<br/>
+                            = {this.state.ronToUsd} USD<br/>
+                            = {this.state.ronToYen} YEN
                         </div>
                     </div>
                 </div>
