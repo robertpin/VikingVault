86de457f61e6df4887d3420223172d38f7ccdfb6
vasi <axinte.vasilica@wirtek.com>
2019-08-12 14:20:55 +0300

    update baseurl

--- ClientApp/viking-vault/src/ConstantVariables.js
+++ ClientApp/viking-vault/src/ConstantVariables.js
@@ -1,5 +1,5 @@
 const constants = {
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://viking-vault.azurewebsites.net/api/api' : "https://localhost:44323/api/"
+    baseUrl: process.env.NODE_ENV === 'production' ? 'https://viking-vault.azurewebsites.net/api/api/' : "https://localhost:44323/api/"
 }
 
 export {constants};
