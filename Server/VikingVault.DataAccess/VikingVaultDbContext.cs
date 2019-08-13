db1780b7684e784508d656c1195140b231b33329
Olah Catalina <olah_catalina@yahoo.com>
2019-08-13 12:08:27 +0300

    Tried to add initial admin

--- Server/VikingVault.DataAccess/VikingVaultDbContext.cs
+++ Server/VikingVault.DataAccess/VikingVaultDbContext.cs
@@ -11,18 +11,40 @@ namespace VikingVault.DataAccess
     {
         public VikingVaultDbContext(DbContextOptions<VikingVaultDbContext> options)
             : base(options)
-        { }
+        {
+        }
         
         public DbSet<User> User { get; set; }
         public DbSet<BankAccount> BankAccount { get; set; }
         public DbSet<Card> Cards { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }  
+        
+        public void SeedAdmin(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().
+                HasData(new User
+                {
+                    Id = 1,
+                    Email = "admin",
+                    Password = "admin",
+                    FirstName = "Admin Firstname",
+                    LastName = "Admin Lastname",
+                    PictureLink = "",
+                    Address = "Ioan Budai Deleanu Street 64",
+                    Cnp = "1700820642466",
+                    Role = "admin"
+                });
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<User>()
                 .HasAlternateKey(c => c.Email)
                 .HasName("Email");
+
+            SeedAdmin(modelBuilder);
         }
-        public DbSet<Transaction> Transactions { get; set; }
     }
 }
