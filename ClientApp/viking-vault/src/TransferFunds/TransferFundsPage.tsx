1c22108c8e844d988cda67287fc10cd528444b5f
radulambrino <radulambrino@yahoo.com>
2019-08-14 09:59:07 +0300

    Basic UI done

--- ClientApp/viking-vault/src/TransferFunds/TransferFundsPage.tsx
+++ ClientApp/viking-vault/src/TransferFunds/TransferFundsPage.tsx
@@ -12,7 +12,9 @@ import '../ExchangeForm.css';
 const url = constants.baseUrl+"TransferFunds";
 
 interface ITransferFundsState{
-    transferedAmount: number
+    transferedAmount: number;
+    cardNumber: number;
+    transferDetails: string;
 }
 
 class TransferFundsPage extends React.Component<any, ITransferFundsState>{
@@ -25,6 +27,36 @@ class TransferFundsPage extends React.Component<any, ITransferFundsState>{
         this.setState({
             transferedAmount: e.target.value
         })
+    }
+
+    handleChangedCardNumber(e : any)
+    {
+        this.setState({
+            cardNumber: e.target.value
+        })
+    }
+
+    handleChangedTransferDetails(e : any)
+    {
+        this.setState({
+            transferDetails: e.target.value
+        })
+    }
+
+    transferMoney(){
+
+        let token = sessionStorage.getItem('Authentication-Token');
+        if(token != null) {
+            fetch("https://localhost:44323/api/bankAccount", {
+                method: "GET",
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  'x-access-token': token.toString()
+                }
+            })
+            .then(response => response.json());
+        }
     }
 
     render(){
@@ -48,21 +80,34 @@ class TransferFundsPage extends React.Component<any, ITransferFundsState>{
                                 <p className="total-balance-text">Total balance</p>
                             </div>
                             
-                            
-                        <div id="amount-container">
-                            <p className="text-decoration">Amount</p>
-                            <input
-                                    className="form-control form-control-currency input-shadow underlineIt"
-                                    placeholder=""
-                                    type="number"
-                                    min="1"
-                                    pattern="^[0-9]"
-                                    max= "900" //{this.state.availableAmountFromCurrency.toString()} TO BE CHANGED
-                                    step="0.01"
-                                    onChange={this.setAmountToBeTransfered}
-                            />
-                        </div>
+                            <div id="amount-container">
+                                <p className="text-decoration">Amount</p>
+                                <input
+                                        className="form-control form-control-currency input-shadow underlineIt"
+                                        placeholder=""
+                                        type="number"
+                                        min="1"
+                                        pattern="^[0-9]"
+                                        max= "900" //{this.state.availableAmountFromCurrency.toString()} TO BE CHANGED
+                                        step="0.01"
+                                        onChange={this.setAmountToBeTransfered}
+                                />
+                            </div>
 
+                            <div className="transfer-details-container">
+                                Details
+                                <form>
+                                    Card Number:<br/>
+                                    <input type="text" name="cardnumber" onChange = {this.handleChangedCardNumber} /><br/>
+                                    Transfer Details:<br/>
+                                    <input type="text" name="transferdetails" onChange = {this.handleChangedTransferDetails} /><br/>
+                                </form>
+
+                                <button onClick = {this.transferMoney}>
+                                    Transfer Now
+                                </button>   
+                            </div>
+                            
                         </div>
                     </div> 
                 </div>
