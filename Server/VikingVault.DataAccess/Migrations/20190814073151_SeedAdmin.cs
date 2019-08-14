07ae582413b1ec8ad1b11bd19ff714f84d203af4
Olah Catalina <olah_catalina@yahoo.com>
2019-08-14 12:01:02 +0300

    Buttons from SideBar redirect to certain pages

---
+++ Server/VikingVault.DataAccess/Migrations/20190814073151_SeedAdmin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VikingVault.DataAccess.Migrations
+{
+    public partial class SeedAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
