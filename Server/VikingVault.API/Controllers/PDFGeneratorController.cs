a1d2ef56586a9a8a1e918a427ac208b4461dc330
Olah Catalina <olah_catalina@yahoo.com>
2019-08-20 16:14:19 +0300

    Added a PDF creator

---
+++ Server/VikingVault.API/Controllers/PDFGeneratorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VikingVault.Services.Abstractions;
+
+namespace VikingVault.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PDFGeneratorController : ControllerBase
+    {
+        private IPDFGeneratorService _pdfGeneratorService;
+
+        public PDFGeneratorController(IPDFGeneratorService pdfGeneratorService)
+        {
+            _pdfGeneratorService = pdfGeneratorService;
+        }
+
+        [HttpGet]
+        public FileStreamResult CreatePDF()
+        {
+            try
+            {
+                var pdf = _pdfGeneratorService.GetTransactionListAsPDF();
+                return pdf;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
