00afb10c745bb56e288b1d07106d73c7ac58dcba
Robert Pintilii <robert.pin9@gmail.com>
2019-08-21 11:53:19 +0300

    Created company service

---
+++ Server/VikingVault.Services/CompanyService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VikingVault.DataAccess;
+using VikingVault.DataAccess.Models;
+using VikingVault.Services.Abstractions;
+using VikingVault.Services.Exceptions;
+
+namespace VikingVault.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly VikingVaultDbContext _dbContext;
+        private readonly IBankAccountService _bankAccountService;
+
+        public CompanyService(VikingVaultDbContext dbContext, IBankAccountService bankAccountService)
+        {
+            _dbContext = dbContext;
+            _bankAccountService = bankAccountService;
+        }
+
+        public User CreateCompany(User company)
+        {
+            company.Role = "company";
+
+            try
+            {
+                _dbContext.Add(company);
+                _bankAccountService.CreateBankAccount(this.CreateBankAccount(company, "Ron"));
+
+                _dbContext.SaveChanges();
+            } catch (Exception e)
+            {
+                if (e is DbUpdateException || e is DbUpdateConcurrencyException || e is BankAccountServiceException)
+                {
+                    throw new CompanyServiceException();
+                }
+            }
+
+            return company;
+        }
+
+        private BankAccount CreateBankAccount(User user, String currencyType)
+        {
+            return new BankAccount
+            {
+                User = user,
+                CurrencyType = currencyType,
+                Balance = 0.0f
+            };
+        }
+    }
+}
---
+++ Server/VikingVault.Services/Exceptions/CompanyServiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VikingVault.Services
+{
+    [Serializable]
+    internal class CompanyServiceException : Exception
+    {
+        public CompanyServiceException()
+        {
+        }
+
+        public CompanyServiceException(string message) : base(message)
+        {
+        }
+
+        public CompanyServiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CompanyServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
