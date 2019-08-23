b4b8c8df5367036c1413937153c0257ba3dac639
radulambrino <radulambrino@yahoo.com>
2019-08-23 14:05:49 +0300

    Just a schematic of the logic flow

---
+++ Server/VikingVault.API/Controllers/TransferRequestsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VikingVault.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    private readonly ITransferRequestsService _transferRequestsService;
+
+    public class TransferRequestsController : ControllerBase
+    {
+        [HttpGet]
+        public getAllTransferRequests()
+        {
+            /** 1) getCardNumberFromToken()
+             *  2) if(cardNumber != null)
+             *     {
+             *         User user = _userService.getUserFromCardNumber(cardNumber);
+             *         var requests = _transferRequestService.getAllRequests(user);
+             *         return requests;
+             *     }
+             *     
+             *  3) ****TRANSFER REQUEST SERVICE*****
+             *     _dbcontext.TransferRequests.Where(request => request.CardNumberReciever == CardNumber).ToList();
+            */
+        }
+
+    }
+}
